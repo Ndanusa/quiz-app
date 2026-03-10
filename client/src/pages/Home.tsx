a72e67e1cc35bf08f0a36e6d58d18e9e0aff0569
Sadeeq Ndanusa <sadeeqndanusa5@gmail.com>
2026-03-10 22:19:07 +0100

    lee

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,28 +2,32 @@ import { useRef, useState, useEffect } from "react";
 import questions from "../data/data.json";
 function Home() {
   const [answers, setAnswers] = useState({});
+  const [questionIndex, setQuestionIndex] = useState(0);
 
   const handleChange = (questionId, optionId) => {
     setAnswers((prev) => ({ ...prev, [questionId]: optionId }));
   };
   const renderQuestions = () => {
     return questions.questions.map((question) => (
-      <div key={question.questionId}>
-        <p>{question.questionText}</p>
-        {question.options.map((option) => (
-          <label key={option.optionId}>
-            <input
-              type="radio"
-              name={question.questionId}
-              value={option.optionId}
-              checked={answers[question.questionId] === option.optionId}
-              onChange={() =>
-                handleChange(question.questionId, option.optionId)
-              }
-            />
-            {option.text}
-          </label>
-        ))}
+      <div>
+        <h1 className="font-bold text-xl">{question.questionId}</h1>
+        <div key={question.questionId}>
+          <p className="text-lg">{question.questionText}</p>
+          {question.options.map((option) => (
+            <label key={option.optionId} className="flex gap-2">
+              <input
+                type="radio"
+                name={question.questionId}
+                value={option.optionId}
+                checked={answers[question.questionId] === option.optionId}
+                onChange={() =>
+                  handleChange(question.questionId, option.optionId)
+                }
+              />
+              <p>{option.text}</p>
+            </label>
+          ))}
+        </div>
       </div>
     ));
   };
