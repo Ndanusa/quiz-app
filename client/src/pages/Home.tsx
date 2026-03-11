49dabf5123ab0381242fcd8038be2519750cdfa0
Sadeeq Ndanusa <sadeeqndanusa5@gmail.com>
2026-03-11 02:58:34 +0100

    for codespace

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,35 +1,11 @@
 import { useRef, useState, useEffect } from "react";
-import questions from "../data/data.json";
+import data from "../data/data.json";
 function Home() {
   const [answers, setAnswers] = useState({});
   const [questionIndex, setQuestionIndex] = useState(0);
-
+  const question = data.questions[questionIndex];
   const handleChange = (questionId, optionId) => {
     setAnswers((prev) => ({ ...prev, [questionId]: optionId }));
-  };
-  const renderQuestions = () => {
-    return questions.questions.map((question) => (
-      <div>
-        <h1 className="font-bold text-xl">{question.questionId}</h1>
-        <div key={question.questionId}>
-          <p className="text-lg">{question.questionText}</p>
-          {question.options.map((option) => (
-            <label key={option.optionId} className="flex gap-2">
-              <input
-                type="radio"
-                name={question.questionId}
-                value={option.optionId}
-                checked={answers[question.questionId] === option.optionId}
-                onChange={() =>
-                  handleChange(question.questionId, option.optionId)
-                }
-              />
-              <p>{option.text}</p>
-            </label>
-          ))}
-        </div>
-      </div>
-    ));
   };
 
   return (
@@ -37,12 +13,58 @@ function Home() {
       <div className="flex flex-col gap-3">
         <div className="text-center ">
           <h1 className="text-2xl text-zinc-800">
-            {questions.title} by{" "}
-            <span className="">{questions.createdBy.username}</span>
+            {data.title} by <span className="">{data.createdBy.username}</span>
           </h1>
-          <p className="text-gray-700">{questions.description}</p>
+          <p className="text-gray-700">{data.description}</p>
         </div>
-        <div>{renderQuestions()}</div>
+        <div>
+          <div>
+            <h1 className="font-bold text-xl">{question.questionId}</h1>
+            <div key={question.questionId}>
+              <p className="text-lg">{question.questionText}</p>
+              {question.options.map((option) => (
+                <label key={option.optionId} className="flex gap-2">
+                  <input
+                    type="radio"
+                    name={question.questionId}
+                    value={option.optionId}
+                    checked={answers[question.questionId] === option.optionId}
+                    onChange={() => {
+                      handleChange(question.questionId, option.optionId);
+                    }}
+                  />
+                  <p>{option.text}</p>
+                </label>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            onClick={() => {
+              questionIndex > 0
+                ? setQuestionIndex(questionIndex - 1)
+                : setQuestionIndex(data.questions.length - 1);
+            }}>
+            prev
+          </button>
+          <button
+            onClick={() => {
+              questionIndex < data.questions.length - 1
+                ? setQuestionIndex(questionIndex + 1)
+                : setQuestionIndex(0);
+            }}>
+            next
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              console.log(answers);
+            }}>
+            Get Answers
+          </button>
+        </div>
       </div>
     </div>
   );
