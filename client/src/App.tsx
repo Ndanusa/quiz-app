bf87d020a219dcb6bbb1f2b2ce86546a652f9b72
sadeeq <sadeeqndanusa5@gmail.com>
2026-04-01 23:36:27 +0100

    link highlighting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, Navigate, useParams } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Login from "./pages/Login";
@@ -14,18 +14,20 @@ import {
   BookIcon,
   DashboardCircleIcon,
   DashboardSquareIcon,
+  DiscoverSquareIcon,
   Logout,
   NoteAddIcon,
   Search02Icon,
   SearchIcon,
   Settings04Icon,
 } from "@hugeicons/core-free-icons";
+import Discover from "./pages/Discover.tsx";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  console.log(useParams());
+  const currentLocation = useLocation().pathname;
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (!token) {
@@ -61,6 +63,7 @@ function App() {
 
     checkAuth();
   }, []);
+  console.log(useLocation());
 
   if (loading) {
     return (
@@ -76,26 +79,30 @@ function App() {
     <>
       <div className={`${isAuth ? "flex " : ""}`}>
         {isAuth && (
-          <nav className="left-0 w-fit flex flex-col sticky gap-20 items-start px-7 pr-15 py-5 bg-[#f8f8f9] border-r border-zinc-400/30 glass h-screen">
+          <nav className="left-0 w-fit flex flex-col sticky gap-20 items-start px-5 pr-15 py-5 bg-[#f8f8f9] border-r border-zinc-400/30 glass h-screen">
             <div className="w-full h-px bg-zinc-200 absolute left-0 top-90"></div>
             <div className="w-full h-px bg-zinc-200 absolute left-0 top-22"></div>
 
             <div>
               <img src={logoImg} alt="" className="w-26" />
             </div>
-            <ul className="flex flex-col items-start justify-between gap-8">
-              <li>
-                <Link to={`/dashboard`} className="flex items-center gap-3">
+            <ul className="flex flex-col items-start justify-between gap-4">
+              <li
+                className={`w-full ${currentLocation === "/dashboard" ? "bg-[#092316]" : ""} px-5 py-2 sqc-md`}
+              >
+                <Link to={`/dashboard`} className={`flex items-center gap-3`}>
                   <HugeiconsIcon
                     icon={DashboardCircleIcon}
                     strokeWidth={1.6}
-                    className="text-zinc-600"
+                    className={`text-zinc-600`}
                     size={18}
                   />
                   <p className=" text-zinc-600 text-sm">Overview</p>
                 </Link>
               </li>
-              <li>
+              <li
+                className={`w-full ${currentLocation === "/create" ? "bg-[#092316]" : ""} px-5 py-2 sqc-md`}
+              >
                 <Link to={`/create`} className="flex items-center gap-3">
                   <HugeiconsIcon
                     icon={NoteAddIcon}
@@ -107,18 +114,22 @@ function App() {
                 </Link>
               </li>
 
-              <li>
-                <Link to={`/search`} className="flex items-center gap-3">
+              <li
+                className={`w-full ${currentLocation === "/explore" ? "bg-[#092316]" : ""} px-5 py-2 sqc-md`}
+              >
+                <Link to={`/discover`} className="flex items-center gap-3">
                   <HugeiconsIcon
-                    icon={SearchIcon}
+                    icon={DiscoverSquareIcon}
                     strokeWidth={1.6}
                     className="text-zinc-600"
                     size={18}
                   />
-                  <p className=" text-zinc-600 text-sm">Search quiz</p>
+                  <p className=" text-zinc-600 text-sm">Discover</p>
                 </Link>
               </li>
-              <li>
+              <li
+                className={`w-full ${currentLocation === "/quizzes" ? "bg-[#092316]" : ""} px-5 py-2 sqc-md`}
+              >
                 <Link to={`/quizzes`} className="flex items-center gap-3">
                   <HugeiconsIcon
                     icon={BookIcon}
@@ -131,8 +142,10 @@ function App() {
               </li>
             </ul>
             <ul className={`flex flex-col gap-8 justify-between`}>
-              <li>
-                <Link to={`/quizzes`} className="flex items-center gap-3">
+              <li
+                className={`w-full ${currentLocation === "/settings" ? "bg-[#092316]" : ""} px-5 py-2 sqc-md`}
+              >
+                <Link to={`/settings`} className="flex items-center gap-3">
                   <HugeiconsIcon
                     icon={Settings04Icon}
                     strokeWidth={1.6}
@@ -188,6 +201,10 @@ function App() {
             element={isAuth ? <CreateQuiz /> : <Navigate to="/login" replace />}
           />
           <Route
+            path="/discover"
+            element={isAuth ? <Discover /> : <Navigate to="/login" replace />}
+          />
+          <Route
             path="/take/:id"
             element={isAuth ? <TakeQuiz /> : <Navigate to="/login" replace />}
           />
@@ -195,6 +212,7 @@ function App() {
             path="/settings"
             element={isAuth ? <Settings /> : <Navigate to="/login" replace />}
           />
+
           <Route
             path="/play"
             element={
---
+++ client/src/pages/Discover.tsx
@@ -0,0 +1,4 @@
+function Discover() {
+  return <div>this is a test</div>;
+}
+export default Discover;
