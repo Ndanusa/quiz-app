f9a60439d5529d3531fa50bbb962db2421f79adf
sadeeq <sadeeqndanusa5@gmail.com>
2026-05-04 19:34:42 +0100

    ui changes for the quiz creating

--- client/src/pages/CreateQuiz.tsx
+++ client/src/pages/CreateQuiz.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { BACKEND_URI } from "../config/config.ts";
 import { useNavigate } from "react-router-dom";
 import CircularProgress from "../components/ProgressBar.tsx";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { Upload03Icon } from "@hugeicons/core-free-icons";
+import {
+  ArrowDownDoubleIcon,
+  ArrowUpDoubleIcon,
+  Upload03Icon,
+} from "@hugeicons/core-free-icons";
 function CreateQuiz() {
-  1;
+  const options = [
+    { value: "private", label: "Private" },
+    { value: "public", label: "Public" },
+  ];
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [privacy, setPrivacy] = useState("public");
+  const [isVisible, setIsVisible] = useState(false);
   const [questions, setQuestions] = useState([
     {
       points: 10,
@@ -23,6 +31,29 @@ function CreateQuiz() {
       correctAnswer: "A",
     },
   ]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(options[0]);
+  const ref = useRef();
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsVisible(true);
+    } else {
+      const timeout = setTimeout(() => setIsVisible(false), 200); // match duration
+      return () => clearTimeout(timeout);
+    }
+  }, [isOpen]);
+
+  // close on outside click
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
   const addQuestion = () => {
     setQuestions((prev) => [
       ...prev,
@@ -84,38 +115,73 @@ function CreateQuiz() {
         <h1 className="text-2xl py-3 ">Create New Quiz</h1>
         <button
           onClick={handleSubmit}
-          className="text-xl flex items-center gap-2 text-[#d1e2db] bg-[#03472b] px-5 py-2 sqc-md"
+          className=" flex items-center cursor-pointer gap-2 bg-[#c3ffe2] text-[#24ad6d] transition-colors duration-300 hover:bg-[#8effc9] hover:text-[#0a6f40] px-5 py-2 sqc-md"
         >
           <HugeiconsIcon icon={Upload03Icon} size={17} />
           Upload
         </button>
       </div>
-      <div className="flex *:py-5 *:px-10">
-        <div className="flex-1 bg-white">
+      <div className="flex item-center gap-10 *:py-5 *:px-10 *:bg-white">
+        <div className="flex-1">
           <div className="flex flex-col">
+            <h1 className="font-medium pt-3 pb-1 px-2">Title</h1>
             <input
-              className="h-10 px-3 border-2 placeholder:text-[#a4a4a4] border-[#c4ccc8] w-7/10 rounded-md sqc-md"
+              className="h-10 px-3 border placeholder:text-[#a4a4a4] placeholder:text-sm placeholder:font-light border-[#c4ccc8] w-7/10 rounded-md sqc-md hover:border-[#48c58a] focus:outline-none focus:ring-2 focus:ring-[#48c58a]"
               onChange={(e) => setTitle(e.target.value)}
               value={title}
               placeholder="Quiz title"
             />
+            <h1 className="font-medium pt-3 pb-1 px-2">Description</h1>
             <textarea
-              className=" placeholder:text-[#a4a4a4] border-2 border-[#c4ccc8] sqc-lg"
+              className=" placeholder:text-[#a4a4a4] placeholder:text-sm placeholder:font-light px-3 py-1 border border-[#c4ccc8] sqc-md hover:border-[#48c58a] focus:outline-none focus:ring-2 focus:ring-[#48c58a]"
               value={description}
               onChange={(e) => setDescription(e.target.value)}
               placeholder="Quiz description"
             ></textarea>
             <div>
-              <h1 className="text-xl">Privacy</h1>
-              <select
-                value={privacy}
-                onChange={(e) => {
-                  setPrivacy(e.target.value);
-                }}
-              >
-                <option value="public">Public</option>
-                <option value="private">Private</option>
-              </select>
+              <h1 className="font-medium pt-3 pb-1 px-2">Privacy</h1>
+              <div className="w-64 relative" ref={ref}>
+                {/* SELECT BUTTON */}
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  className=" w-8/10 flex justify-between text-sm items-center px-4 py-2 bg-white border border-[#c4ccc8] sqc-md text-[#a4a4a4] hover:border-[#48c58a] focus:outline-none focus:ring-2 focus:ring-[#48c58a]"
+                >
+                  <span className="text-sm">{selected.label}</span>
+                  <span
+                    className={`transition-transform ${isOpen ? "rotate-180" : ""}`}
+                  >
+                    <HugeiconsIcon icon={ArrowDownDoubleIcon} size={18} />
+                  </span>
+                </button>
+
+                {/* DROPDOWN OPTIONS */}
+                {isOpen && (
+                  <ul
+                    className={`absolute mt-2 w-6/10 bg-white border p-3 transform transition-all duration-200 origin-top border-gray-200 sqc-lg shadow-lg overflow-hidden z-10 ${
+                      isOpen
+                        ? "opacity-100 scale-100 translate-y-0"
+                        : "opacity-0 scale-95 -translate-y-2"
+                    }`}
+                  >
+                    {options.map((option) => (
+                      <li
+                        key={option.value}
+                        onClick={() => {
+                          setSelected(option);
+                          setIsOpen(false);
+                        }}
+                        className={` px-4 py-2 cursor-pointer text-sm transition-colors ${
+                          selected.value === option.value
+                            ? "bg-[#c3ffe2] text-[#24ad6d] sqc-sm"
+                            : "text-gray-700 hover:bg-[#eeeeee]"
+                        } `}
+                      >
+                        {option.label}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
           </div>
         </div>
